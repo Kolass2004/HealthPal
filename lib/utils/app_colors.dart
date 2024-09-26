b82e39bb2e12abdce123c2a3bb48a5b6d3639c5a
aconitum001 <skandern0000@gmail.com>
2024-09-26 19:20:58 +0100

    added app colors

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // main
+  static const primaryColor = Color(0xff1C2A3A);
+  static const secondaryColor = Color(0xffFFFFFF);
+  // grey scale
+  static const grey50 = Color(0xffF9FAFB);
+  static const grey100 = Color(0xffF3F4F6);
+  static const grey200 = Color(0xffE5E7EB);
+  static const grey300 = Color(0xffD1D5DB);
+  static const grey400 = Color(0xff9CA3AF);
+  static const grey500 = Color(0xff6B7280);
+  static const grey600 = Color(0xff4B5563);
+  static const grey700 = Color(0xff374151);
+  static const grey800 = Color(0xff1F2A37);
+  static const grey900 = Color(0xff11928);
+}
