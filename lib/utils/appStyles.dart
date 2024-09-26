ec452e3ae8790c3a728144ce5bdc4c77f3a4cfc5
aconitum001 <skandern0000@gmail.com>
2024-09-26 19:11:27 +0100

    added appstyles

---
+++ lib/utils/appStyles.dart
@@ -0,0 +1,62 @@
+import 'dart:ui';
+
+import 'package:health_pal/utils/constants.dart';
+
+abstract class Appstyles {
+  static TextStyle styleRegular14 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 14),
+    fontWeight: FontWeight.normal,
+    fontFamily: fontFamily,
+  );
+
+   static TextStyle styleMedium14 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 14),
+    fontWeight: FontWeight.w500,
+    fontFamily: fontFamily,
+  );
+
+  static TextStyle styleRegular20 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 20),
+    fontWeight: FontWeight.normal,
+    fontFamily: fontFamily,
+  );
+
+   static TextStyle styleBold18 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 18),
+    fontWeight: FontWeight.bold,
+    fontFamily: fontFamily,
+  );
+
+   static TextStyle styleMedium16 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 16),
+    fontWeight: FontWeight.w500,
+    fontFamily: fontFamily,
+  );
+
+   static TextStyle styleSemiBold20 = TextStyle(
+    fontSize: getResponsiveFontSize(fontsize: 20),
+    fontWeight: FontWeight.w600,
+    fontFamily: fontFamily,
+  );
+}
+
+double getResponsiveFontSize({required double fontsize}) {
+  double scaleFactor = getScaleFactor(fontsize);
+  double responsiveFontSize = fontsize * scaleFactor;
+
+  double lowerLimit = fontsize * 0.5;
+  double upperLimit = fontsize * 1.3;
+
+  responsiveFontSize = responsiveFontSize.clamp(lowerLimit, upperLimit);
+
+  return responsiveFontSize;
+}
+
+double getScaleFactor(double fontsize) {
+  var dispatcher = PlatformDispatcher.instance;
+  var physicalWidth = dispatcher.views.first.physicalSize.width;
+  var devicePixelRatio = dispatcher.views.first.devicePixelRatio;
+  var width = physicalWidth / devicePixelRatio;
+
+  return width / 380;
+}
