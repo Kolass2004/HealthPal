f781e6c14adc4a89fae7ebc6885a5ffa2905d5f3
aconitum001 <skandern0000@gmail.com>
2024-09-26 18:43:59 +0100

    added app assets

---
+++ lib/utils/assets.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsFirstDoctor
+  /// assets/first_doctor.png
+  static const String assetsFirstDoctor = "assets/first_doctor.png";
+
+  /// Assets for assetsLogoDark
+  /// assets/logo_dark.svg
+  static const String assetsLogoDark = "assets/logo_dark.svg";
+
+  /// Assets for assetsLogoWhite
+  /// assets/logo_white.svg
+  static const String assetsLogoWhite = "assets/logo_white.svg";
+
+  /// Assets for assetsSecoundDoctor
+  /// assets/secound_doctor.png
+  static const String assetsSecoundDoctor = "assets/secound_doctor.png";
+
+  /// Assets for assetsSplash
+  /// assets/Splash.png
+  static const String assetsSplash = "assets/Splash.png";
+
+  /// Assets for assetsThirdDoctor
+  /// assets/third_doctor.png
+  static const String assetsThirdDoctor = "assets/third_doctor.png";
+}
+
