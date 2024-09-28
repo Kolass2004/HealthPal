c08b26b0857e6f13f9f19fe21b801936b34cec47
aconitum001 <skandern0000@gmail.com>
2024-09-28 16:22:57 +0100

    created sign in view

---
+++ lib/views/sign_in_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:health_pal/utils/app_colors.dart';
+import 'package:health_pal/widgets/sign_in_view_body.dart';
+
+class SignInView extends StatelessWidget {
+  const SignInView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: AppColors.secondaryColor,
+      body: SignInViewBody(),
+    );
+  }
+}
