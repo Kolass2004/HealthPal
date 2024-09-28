9b56ca1cd946c5ce5a4406f894f4c1145f015211
aconitum001 <skandern0000@gmail.com>
2024-09-28 16:31:08 +0100

    created routing for sign in

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:health_pal/views/sign_in_view.dart';
 import 'package:health_pal/views/sign_up_view.dart';
 import 'package:health_pal/views/on_boarding_view.dart';
 import 'package:health_pal/views/splash_view.dart';
 
 abstract class AppRouter {
   static const String onBoardingView = "/onBoardingView";
-  static const String loginView = "/loginView";
+  static const String signUpView = "/signUpView";
+  static const String signInView = "/signInView";
 
   static final router = GoRouter(
     routes: [
@@ -38,11 +40,36 @@ abstract class AppRouter {
         },
       ),
       GoRoute(
-        path: loginView,
+        path: signUpView,
         pageBuilder: (context, state) {
           return CustomTransitionPage(
             key: state.pageKey,
             child: const SignUpView(),
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              const beginOffset = Offset(1.0, 0.0); // Slide in from the right
+              const endOffset = Offset(0.0, 0.0);
+              const curve = Curves.easeInOut;
+
+              var tween = Tween(begin: beginOffset, end: endOffset).chain(
+                CurveTween(curve: curve),
+              );
+              var offsetAnimation = animation.drive(tween);
+
+              return SlideTransition(
+                position: offsetAnimation,
+                child: child,
+              );
+            },
+          );
+        },
+      ),
+      GoRoute(
+        path: signInView,
+        pageBuilder: (context, state) {
+          return CustomTransitionPage(
+            key: state.pageKey,
+            child: const SignInView(),
             transitionsBuilder:
                 (context, animation, secondaryAnimation, child) {
               const beginOffset = Offset(1.0, 0.0); // Slide in from the right
--- lib/widgets/sign_in_view_body.dart
+++ lib/widgets/sign_in_view_body.dart
@@ -1,10 +1,132 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:health_pal/utils/app_colors.dart';
+import 'package:health_pal/utils/app_router.dart';
+import 'package:health_pal/utils/app_styles.dart';
+import 'package:health_pal/utils/assets.dart';
+import 'package:health_pal/widgets/custom_button.dart';
+import 'package:health_pal/widgets/logo_text.dart';
+import 'package:health_pal/widgets/or_line_widget.dart';
+import 'package:health_pal/widgets/sign_in_text_fields.dart';
+import 'package:health_pal/widgets/social_media_button.dart';
+import 'package:health_pal/widgets/switch_between_sign_in_up.dart';
 
 class SignInViewBody extends StatelessWidget {
   const SignInViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          SvgPicture.asset(
+            Assets.assetsLogoDark,
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          const LogoText(
+            color1: AppColors.grey500,
+            color2: AppColors.grey900,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          Text(
+            "Hi, Welcome Back!",
+            style: Appstyles.styleSemiBold20.copyWith(
+              color: AppColors.primaryColor,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            "Hope you're doing fine.",
+            style: Appstyles.styleRegular14.copyWith(
+              color: AppColors.grey500,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          const SignInTextFields(),
+          const SizedBox(
+            height: 24,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: SizedBox(
+              height: 48,
+              child: CustomButton(
+                title: "Sign In",
+                onPressed: () {},
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          const OrLineWidget(),
+          const SizedBox(
+            height: 21,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: SocialMediaButton(
+              title: "Sign in with Google",
+              onPressed: () {},
+              icon: Assets.assetsGoogleOriginal,
+              isGoogle: true,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: SocialMediaButton(
+              title: "Sign in with Facebook",
+              onPressed: () {},
+              icon: Assets.assetsFacebook,
+              isGoogle: false,
+            ),
+          ),
+          const SizedBox(
+            height: 23,
+          ),
+          InkWell(
+            onTap: () {},
+            child: Text(
+              "Forgot password? ",
+              style: Appstyles.styleMedium14.copyWith(
+                color: const Color(0xff1C64F2),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          const SizedBox(
+            height: 23,
+          ),
+          SwitchBetweenSignInUpText(
+            text: "Do you have an account yet?",
+            page: "Sign Up",
+            onPressed: () {
+              GoRouter.of(context).push(AppRouter.loginView)
+            },
+          ),
+          const SizedBox(
+            height: 21,
+          ),
+        ],
+      ),
+    );
   }
 }
